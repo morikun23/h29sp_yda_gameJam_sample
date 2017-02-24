eb5b210511c9dd62a2719938087afc8db6e79b28
MoritaSuguru <morisugood@gmail.com>
2017-02-25 00:19:13 +0900

    【Chapter.6】
    ・アニメーションの表示を実装しました。
    ・アニメーション用のスクリプトを追加しました。
    ・Playerクラスのリファクタリング

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,12 @@ public class Player : MonoBehaviour {
 
 	float speed = 0.1f;
 	Rigidbody2D rigidbody2d;
+	Vector3 velocity;
 
+	[SerializeField]
+	bool isRunning;
+	[SerializeField]
+	bool isGrounded;
 
 	// Use this for initialization
 	void Start() {
@@ -15,27 +20,66 @@ public class Player : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-		///////////////////////////////////////////////////////
-		//Chapter1
-		
-		if (Input.GetKey(KeyCode.RightArrow)) {
-			transform.position += new Vector3(speed , 0);
-		}
-		if (Input.GetKey(KeyCode.LeftArrow)) {
-			transform.position += new Vector3(-speed , 0);
-		}
 
-		///////////////////////////////////////////////////////
-
+		Run();
 
 		///////////////////////////////////////////////////////
 		//Chapter5
-		if (Physics2D.Raycast(transform.position , Vector2.down , 2.0f , 1 << LayerMask.NameToLayer("Ground"))){
-			if (Input.GetKeyDown(KeyCode.Space)) {
-				rigidbody2d.AddForce(new Vector2(0 , 400f));
-			}
+		isGrounded = Physics2D.Linecast(transform.position ,
+			transform.position - transform.up * 1.6f ,
+			1 << LayerMask.NameToLayer("Ground"));
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Jump();
 		}
+		
+	}
+
+	void Run() {
+		///////////////////////////////////////////////////////
+		//Chapter1
+
+		//if (Input.GetKey(KeyCode.RightArrow)) {
+		//	transform.position += new Vector3(speed , 0);
+		//}
+		//if (Input.GetKey(KeyCode.LeftArrow)) {
+		//	transform.position += new Vector3(-speed , 0);
+		//}
+
+		///////////////////////////////////////////////////////
+
+		///////////////////////////////////////////////////////
+		//Chapter6
+
+		velocity = Vector3.zero;
+
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			velocity.x += speed;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			velocity.x += -speed;		}
+
+		transform.position += velocity;
+		isRunning = !(velocity.magnitude == 0);
+
 		///////////////////////////////////////////////////////
 	}
-	
+
+	void Jump() {
+		if (!IsGrounded()) return;
+		isGrounded = false;
+		rigidbody2d.AddForce(new Vector2(0 , 400f));
+	}
+
+	public bool IsRunning() {
+		return isRunning;
+	}
+
+	public Vector3 GetDirection() {
+		return velocity.normalized;
+	}
+
+	public bool IsGrounded() {
+		return isGrounded;
+	}	
 }
---
+++ Assets/Scripts/PlayerAnimationViewer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationViewer : MonoBehaviour {
+
+	Player player;
+	Animator animator;
+	SpriteRenderer spriteRenderer;
+	
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Player>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.GetDirection().x > 0) {
+			//右向き
+			spriteRenderer.flipX = false;
+		}
+		else if(player.GetDirection().x < 0) {
+			//左向き
+			spriteRenderer.flipX = true;
+		}
+
+		if (!player.IsGrounded()) {
+			animator.Play("Jump");
+		}
+		else if (player.IsRunning()) {
+			animator.Play("Running");
+		}
+		else {
+			animator.Play("Idling");
+		}
+
+		
+	}
+}
