8f9e3d5ed551f0df9f19e6fb57f62116758d0008
MoritaSuguru <morisugood@gmail.com>
2017-02-25 01:00:34 +0900

    【Chapter.7】
    ・コインの素材を追加しました。
    ・コインのプレハブを追加しました。
    ・コインのスクリプトを追加しました。

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	//取得された
+	bool isGotten;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		isGotten = true;
+	}
+
+	public bool IsGotten() {
+		return isGotten;
+	}
+}
